d6d06c94ef9214c7b67e5388daf671ba7538a9de
smithsteph <stephanie-k-smith@uiowa.edu>
2015-10-08 06:27:34 +0000

    fda

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,28 @@
 class UsersController < ApplicationController
 
     def user_params
-        params.require(:user).permit(:user_id, :email,)
+        params.require(:user).permit(:user_id, :email,:session_token)
     end
     def new
     # default: render 'new' template
     end
 
     def create
-        #user_id=params[:user_id]
+        user_id=params[:user_id]
         redirect_to new
 =begin
         if User.find(user_id).blank?
             flash[:notice] = "Sorry. This user-id is taken. Try again"
             redirect_to new
         else
+=end
             #in database
             @user=CreateUser!({:user_id=>params[:user_id], :email=>params[:email]})
             flash[:notice] = "Welcome #{@user.title} Your account has been created"
             redirect_to movies_path
 
-        end
-=end
+        #end
+
         #add to the User database
     end
 end
