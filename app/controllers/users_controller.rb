6bc0804be971f4e238b12ca83ed075dbee6a44a4
smithsteph <stephanie-k-smith@uiowa.edu>
2015-10-08 06:15:44 +0000

    fda

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
     def create
         user_id=params[:user_id]
+=begin
         if User.find(user_id).blank?
             flash[:notice] = "Sorry. This user-id is taken. Try again"
             redirect_to new
@@ -17,6 +18,7 @@ class UsersController < ApplicationController
             @user=CreateUser!({:user_id=>params[:user_id], :email=>params[:email]})
             flash[:notice] = "Welcome #{@user.title} Your account has been created"
             redirect_to movies_path
+=end
         end
         #add to the User database
     end
