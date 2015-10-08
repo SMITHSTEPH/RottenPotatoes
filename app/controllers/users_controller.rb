a99c4ceaab21979bba7c329cfc5a8b6e8c866669
smithsteph <stephanie-k-smith@uiowa.edu>
2015-10-08 07:15:21 +0000

    fda

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,16 @@ class UsersController < ApplicationController
 
     def create
         user_id=params[:user_id]
+        puts "User"
      
-        puts "IN USER CREAT"
+        puts "IN USER CREATE"
 
         if User.find(user_id).blank?
+            puts "IN CREATE USER IF"
             flash[:notice] = "Sorry. This user-id is taken. Try again"
             redirect_to new
         else
-
+            puts "IN CREATE USER ELSE"
             #in database
             @user=CreateUser!({:user_id=>params[:user_id], :email=>params[:email]})
             flash[:notice] = "Welcome #{@user.title} Your account has been created"
