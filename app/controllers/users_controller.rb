8153e41111315b556a31ab4414ef13c6aa28cd14
smithsteph <stephanie-k-smith@uiowa.edu>
2015-10-09 18:37:15 +0000

    fdsfa

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,12 @@ class UsersController < ApplicationController
     def create
         user_id=user_params[:signup_id]
         puts "In Create"
-        if User.find_by_signup_id(user_id).blank?
+        if User.find_by_user_id(user_id).blank?
             puts "In Create user if"
             @user=User.CreateUser!({:signup_id=>user_params[:signup_id], :signup_email=>user_params[:signup_email]})
             puts "@user"
             puts "@user: " + @user.to_s
-            flash[:notice] = "Welcome #{@user.signup_id} Your account has been created"
+            flash[:notice] = "Welcome #{@user.user_id} Your account has been created"
             redirect_to login_path
         else
             flash[:notice] = "Sorry. This user-id is taken. Try again"
