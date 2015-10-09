0eea0b5f85be5b190d3a6d134a21167b855cc4a9
smithsteph <stephanie-k-smith@uiowa.edu>
2015-10-09 17:53:30 +0000

    fdsfa

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,13 @@ class SessionsController < ApplicationController
     layout false  #overriding application layout
     
     def new
-       
     end
     
     def destroy
     end
     
     def create
+        redirect_to movies_path
     end
 
 end
