2f72c30744e0a35eef39e0bef01e7611099a1621
smithsteph <stephanie-k-smith@uiowa.edu>
2015-10-09 16:36:30 +0000

    starting part three

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
     #layout "new" #overriding application layout
     def new
-        render :layout => "layout_for_new_only"
     end
     
     def destroy
