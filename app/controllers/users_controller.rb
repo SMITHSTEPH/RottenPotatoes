0431fd9452d20da1c9fee1999a3c05b1887105f0
smithsteph <stephanie-k-smith@uiowa.edu>
2015-10-09 17:08:51 +0000

    fdsfa

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
         if User.find_by_user_id(user_id).blank?
             @user=User.CreateUser!({:user_id=>user_params[:user_id], :email=>user_params[:email]})
             flash[:notice] = "Welcome #{@user.user_id} Your account has been created"
-            redirect_to login_path
+            redirect_to new_login_path
         else
             flash[:notice] = "Sorry. This user-id is taken. Try again"
             redirect_to new_user_path
