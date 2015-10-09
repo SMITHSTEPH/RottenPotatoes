462ba0db15474bf3e950aab74c5b224d4aee42e3
smithsteph <stephanie-k-smith@uiowa.edu>
2015-10-09 21:01:42 +0000

    p3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,7 +25,7 @@ class SessionsController < ApplicationController
         else
             puts 'current user id' + user.session_token
             session[:session_token]=user.session_token
-            flash[:notice]="Logged in as#{user.user_id}"
+            #flash[:notice]="Logged in as #{user.user_id}"
             redirect_to movies_path
         end
     end
